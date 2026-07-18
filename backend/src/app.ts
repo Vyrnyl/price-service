0d4d1869b52d107941d880ece852933993b3a423
Vern <aquinovern15@gmail.com>
2026-07-18 22:51:45 +0800

    test: URL

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,46 +1,49 @@
-import express, { Request, Response } from 'express';
-import path from 'path';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import path from "path";
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import apiRoutes from './routes';
-import { errorHandler } from './middleware/error.middleware';
-import { authenticate } from './middleware/auth.middleware';
-import authRoutes from './modules/auth/auth.routes';
-import publicRoutes from './modules/public/public.routes';
+import apiRoutes from "./routes";
+import { errorHandler } from "./middleware/error.middleware";
+import { authenticate } from "./middleware/auth.middleware";
+import authRoutes from "./modules/auth/auth.routes";
+import publicRoutes from "./modules/public/public.routes";
 
 const app = express();
 const port = process.env.PORT || 5000;
-const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:3000';
+const corsOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
 
-const reportsDir = path.resolve(process.cwd(), 'reports');
+const reportsDir = path.resolve(process.cwd(), "reports");
 
-console.log("process.env.CORS_ORIGIN =", process.env.CORS_ORIGIN);
-console.log("corsOrigin =", corsOrigin);
-
-app.use(cors({
+app.use(
+  cors({
     origin: corsOrigin,
     credentials: true,
-}));
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/public', publicRoutes);
-app.use('/api', authenticate, apiRoutes);
-app.use('/reports/files', express.static(reportsDir));
+app.use("/api/auth", authRoutes);
+app.use("/api/public", publicRoutes);
+app.use("/api", authenticate, apiRoutes);
+app.use("/reports/files", express.static(reportsDir));
 
-app.get('/', (_req: Request, res: Response) => {
-  res.json({ message: 'PresyoSerbisyo backend is running' });
+app.get("/", (_req: Request, res: Response) => {
+  console.log("process.env.CORS_ORIGIN =", process.env.CORS_ORIGIN);
+  console.log("corsOrigin =", corsOrigin);
+  res.json({ message: "PresyoSerbisyo backend is running sss" });
 });
 
 app.use((_req: Request, res: Response) => {
-  res.status(404).json({ error: 'Not found' });
+  res.status(404).json({ error: "Not found" });
 });
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}, corsOrigin: ${corsOrigin}`);
+  console.log(
+    `Server running on http://localhost:${port}, corsOrigin: ${corsOrigin}`,
+  );
 });
